3c36c1205692a883c7e2c1a73977aed94885b1fc
zhangbo <Boz_zhang_yl@163.com>
2016-12-02 16:23:54 +0800

    create-vp4

---
+++ app/src/main/java/ckt/base/VP4.java
@@ -0,0 +1,32 @@
+package ckt.base;
+
+/**
+ * Created by admin on 2016/12/2.
+ */
+
+public class VP4 extends VP2 {
+    public VP4(){
+    }
+    public void initDoro(){//wt
+
+    }
+    public void unlock(){//wt
+
+    }
+    public void openAppliction(){//zb
+
+    }
+    //Home page, menu page, applist page
+    public void switchToHomePage(){//zb
+
+    }
+    public void switchToMenuPage(){//zb
+
+    }
+    public void switchToApplistPage(){//zb
+
+    }
+    public void iWantTo(){//dlj
+
+    }
+}
