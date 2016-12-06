7f09a2c30fb2e79f11947fb8d5ef78247911913c
lingjiang.du <lingjiang.du@ck-telecom.com>
2016-12-06 19:24:27 +0800

    Add AlarmCase

--- app/src/androidTest/java/doro/testcase/AlarmCase.java
+++ app/src/androidTest/java/doro/testcase/AlarmCase.java
@@ -1,8 +1,23 @@
 package doro.testcase;
 
+import com.squareup.spoon.Spoon;
+
+import org.junit.Test;
+
+import static ckt.base.VP.gDevice;
+import static ckt.base.VP.initDevice;
+import static ckt.base.VP2.clickByText;
+
 /**
  * Created by user on 2016/12/06   .
  */
 
 public class AlarmCase {
+    @Test
+    public void testA(){
+        initDevice();
+        gDevice.pressHome();
+        clickByText("Settings");
+        Spoon.screenshot("takeScreenShot","Settings");
+    }
 }
