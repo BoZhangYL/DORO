5dd40e3563ba595c851fe2e10c59b90ecae78d25
lingjiang.du <lingjiang.du@ck-telecom.com>
2016-12-08 20:06:48 +0800

    AlarmAction

--- app/src/androidTest/java/doro/action/AlarmAction.java
+++ app/src/androidTest/java/doro/action/AlarmAction.java
@@ -7,5 +7,32 @@ import ckt.base.VP4;
  */
 
 public class AlarmAction extends VP4 {
-
+    /*
+    * 进入与退出闹钟界面，反复执行 counts 次。
+    * */
+    public void enterAndExitAlarm(int counts){
+        try{
+            for(int i=0;i<counts;i++){
+                pressKey("back");
+                getObjectByTextContains("Alarm").clickAndWaitForNewWindow();
+            }
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void addTimeAlarm(String hours,String mins){
+        try{
+            getObjectByTextContains("Add alarm").click();
+            Thread.sleep(2000);
+            getObjectById("com.doro.apps.alarm:id/time_field").click();
+            specialTime(hours,mins);
+            getObjectByTextContains("Confirm").click();
+            getObjectByTextContains("Next").click();
+            getObjectByTextContains("Save").click();
+            Thread.sleep(6000);
+            gDevice.pressBack();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
 }
