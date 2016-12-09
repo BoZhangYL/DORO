1a7bd990bb4a569abe641c484b6080bba38fd8a2
lingjiang.du <lingjiang.du@ck-telecom.com>
2016-12-09 20:54:55 +0800

    Add AlarmPage

---
+++ app/src/androidTest/java/doro/page/AlarmPage.java
@@ -0,0 +1,13 @@
+package doro.page;
+
+/**
+ * Created by user on 2016/12/09   .
+ */
+
+public class AlarmPage {
+    public static final String ALARM_ID_TEXT_ID="com.doro.apps.alarm:id/text";
+    public static final String ALARM_FREQUENCY_FIELD_ID ="com.doro.apps.alarm:id/frequency_field";
+    public static final String ALARM_CLICK_CONFIRM_TEXT ="Confirm";
+    public static final String ALARM_CLICK_NEXT_TEXT ="Next";
+    public static final String ALARM_CLICK_SAVE_TEXT ="Save";
+}
