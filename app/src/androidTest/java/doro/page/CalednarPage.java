f17ceeb5bddf59ecff81bb824c417d00c8686dad
ZhangBo <bo_zhang_YL@163.com>
2016-12-09 13:44:39 +0800

    remove-compileOptions

--- app/src/androidTest/java/doro/page/CalednarPage.java
+++ app/src/androidTest/java/doro/page/CalednarPage.java
@@ -50,10 +50,120 @@ public class CalednarPage {
     //事件重复性选择按钮
 
     public static final String CALENDAR_PACKAGE = "com.doro.apps.calendar";
-    //
+    //日历包名
+
     public static final String DORO_HIDE_KEYBOARD_BUTTON =
             "com.doro.apps.calendar:id/hide_keyboard_button";
     //隐藏键盘按钮
+
+    public static final String CALENDAT_SET_DATE_VIEW =
+            "com.doro.apps.calendar:id/calendarView";
+    //日期设置界面
+
+    public static final String CALENDAR_SET_DAY_VIEW = "android.widget.CheckedTextView";
+    //日期设置日界面
+
+    public static final String CALENDAR_ALL_DAY_ENALE ="1";
+    //启用全天事件
+
+    public static final String CALENDAR_ALL_DAY_DISABLE ="0";
+    //禁用全天事件
+
+    public static final String CALENDAR_ALL_DAY_EVENT =
+            "com.doro.apps.calendar:id/checkbox_whole_day";
+    //全天事件按钮
+
+    public static final String CALENDAR_RECURRENCE_NONE = "None";
+    //循环:None
+
+    public static final String CALENDAR_RECURRENCE_EVERY_DAY ="Every day";
+    //循环:Every day
+
+    public static final String CALENDAR_RECURRENCE_EVERY_WEEK = "Every week";
+    //循环Every week
+
+    public static final String CALENDAR_RECURRENCE_EVERY_MONTH = "Every month";
+    //循环Every month
+
+    public static final String CALENDAR_RECURRENCE_EVERY_YEAR = "Every year";
+    //循环Every year
+
+    public static final String CALENDAR_SET_RECURRENCE_BUTTON =
+            "com.doro.apps.calendar:id/text";
+    //设置循环事件按钮
+    public static final String CALENDAR_RECURRENCE_BUTTON =
+            "com.doro.apps.calendar:id/event_recurrence";
+    //循环事件按钮
+
+    public static final String CALENDAR_SET_HOUR =
+            "com.doro.apps.calendar:id/hour_edit";
+    //设置小时编辑框
+
+    public static final String CALENDAR_SET_MIBUTE =
+            "com.doro.apps.calendar:id/minute_edit";
+    //设置分钟编辑框
+
+    public static final String CALENDAR_ADD_HOUR =
+            "com.doro.apps.calendar:id/hour_increase";
+    //加一小时
+
+    public static final String CALENDAR_MINUS_HOUR =
+            "com.doro.apps.calendar:id/hour_decrease";
+    //减一小时
+
+    public static final String CALENDAR_ADD_MINUTE =
+            "com.doro.apps.calendar:id/minute_increase";
+    //加一分钟
+
+    public static final String CALENDAR_MINUS_MINUTE =
+            "com.doro.apps.calendar:id/minute_decrease";
+    //减一分钟
+
+    public static final String CALENDAR_EDIT_TIME_VIEW_CONFIRM_BUTTON =
+            "com.doro.apps.calendar:id/confirm_text";
+    //confirm按钮
+
+    public static final String CALENDAR_REMINDER_BUTTON =
+            "com.doro.apps.calendar:id/event_alarm_enabler";
+    //Reminder按钮
+
+    public static final String CALENDAR_CHOSE_REMINDER_TYPE =
+            "com.doro.apps.calendar:id/text";
+    //设置reminder值
+
+
+    public static final String CALENDAR_BEFORE_BUTTON =
+            "com.doro.apps.calendar:id/event_alarm";
+    //before按钮
+
+    public static final String CALENDAR_SET_BEFOR = "com.doro.apps.calendar:id/text";
+
+    public static final String CALENDAR_REMINDER_TYPE_NO_REMINDER = "No reminder";
+    public static final String CALENDAR_REMINDER_TYPE_NOTIFICATION = "Notification";
+    public static final String CALENDAR_REMINDER_TYPE_SLARM ="Alarm";
+    //reminder的类型
+
+    public static final String CALENDAR_BEFORE_VALUE_AT_ACTIVITY_TIME =
+            "At activity time";
+    public static final String CALENDAR_BEFORE_VALUE_5_MINUTES = "5 minutes";
+    public static final String CALENDAR_BEFORE_VALUE_15_MINUTES = "15 minutes";
+    public static final String CALENDAR_BEFORE_VALUE_30_MINUTES = "30 minutes";
+    public static final String CALENDAR_BEFORE_VALUE_1_HOUR = "1 hour";
+    public static final String CALENDAR_BEFORE_VALUE_2_HOURS = "2 hours";
+    public static final String CALENDAR_BEFORE_VALUE_1_DAY = "1 day";
+    public static final String CALENDAR_BEFORE_VALUE_2_DAYS ="2 days";
+    //Before的值
+
+
+
+
+
+
+
+
+
+
+
 
 
 
