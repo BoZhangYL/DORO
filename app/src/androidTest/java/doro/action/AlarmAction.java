68126b701aee6325a2e03db201a6bd0b1965a164
lingjiang.du <lingjiang.du@ck-telecom.com>
2016-12-12 13:55:29 +0800

    Modify SpecialTime

--- app/src/androidTest/java/doro/action/AlarmAction.java
+++ app/src/androidTest/java/doro/action/AlarmAction.java
@@ -2,21 +2,26 @@ package doro.action;
 
 import ckt.base.VP4;
 
+import static doro.page.AlarmPage.ALARM_CLICK_ADD_ALARM_TEXT;
 import static doro.page.AlarmPage.ALARM_CLICK_CONFIRM_TEXT;
 import static doro.page.AlarmPage.ALARM_CLICK_NEXT_TEXT;
 import static doro.page.AlarmPage.ALARM_CLICK_SAVE_TEXT;
 import static doro.page.AlarmPage.ALARM_FREQUENCY_FIELD_ID;
+import static doro.page.AlarmPage.ALARM_HOUR_DECREASE_ID;
+import static doro.page.AlarmPage.ALARM_HOUR_EDIT_ID;
+import static doro.page.AlarmPage.ALARM_HOUR_INCREASE_ID;
 import static doro.page.AlarmPage.ALARM_ID_TEXT_ID;
+import static doro.page.AlarmPage.ALARM_MINUTE_DECREASE_ID;
+import static doro.page.AlarmPage.ALARM_MINUTE_EDIT_ID;
+import static doro.page.AlarmPage.ALARM_MINUTE_INCREASE_ID;
+import static doro.page.AlarmPage.ALARM_TIME_FIELD_ID;
 
 /**
  * Created by Lingjiang.Du on 2016/12/07.
  */
 
 public class AlarmAction extends VP4 {
-    /*
-    * 进入与退出闹钟界面，反复执行 counts 次。
-    * */
-    public void enterAndExitAlarm(int counts){
+    public void enterAndExitAlarm(int counts){ //进入与退出闹钟界面，反复执行 counts 次。
         try{
             for(int i=0;i<counts;i++){
                 pressKey("back");
@@ -26,18 +31,48 @@ public class AlarmAction extends VP4 {
             e.printStackTrace();
         }
     }
-    /*
-    * 添加一个指定时间的闹钟,注意时间格式是String类型
-    * */
-    public void addTimeAlarm(String hours,String mins){
+    public void specialTime(String time){//得到指定的时间
+        String[] hourMinTime = time.split(":");
         try{
-            getObjectByTextContains("Add alarm").click();
-            Thread.sleep(2000);
-            getObjectById("com.doro.apps.alarm:id/time_field").click();
-            specialTime(hours,mins);
-            getObjectByTextContains("Confirm").click();
-            getObjectByTextContains("Next").click();
-            getObjectByTextContains("Save").click();
+            String hour =getObjectById(ALARM_HOUR_EDIT_ID).getText();
+            int h = Integer.valueOf(hour);
+            String min =getObjectById(ALARM_MINUTE_EDIT_ID).getText();
+            int m = Integer.valueOf(min);
+            int countH =h-Integer.valueOf(hourMinTime[0]);
+            int countM =m-Integer.valueOf(hourMinTime[1]);
+            if(countH<0){
+                for(int i=0;i<countH;i++){
+                    getObjectById(ALARM_HOUR_INCREASE_ID).click();
+                }
+            }else{
+                for(int j=0;j<countH;j++) {
+                    getObjectById(ALARM_HOUR_DECREASE_ID).click();
+                }
+            }
+            if(countM<0){
+                for(int i=0;i<countM;i++){
+                    getObjectById(ALARM_MINUTE_INCREASE_ID).click();
+                }
+            }else{
+                for(int j=0;j<countM;j++){
+                    getObjectById(ALARM_MINUTE_DECREASE_ID).click();
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    /*
+    * 增加一个指定时间的闹钟
+    * */
+    public void addTimeAlarm(String time ){
+        try{
+            getObjectByTextContains(ALARM_CLICK_ADD_ALARM_TEXT).clickAndWaitForNewWindow();
+            getObjectById(ALARM_TIME_FIELD_ID).clickAndWaitForNewWindow();
+            specialTime(time);
+            getObjectByTextContains(ALARM_CLICK_CONFIRM_TEXT).clickAndWaitForNewWindow();
+            getObjectByTextContains(ALARM_CLICK_NEXT_TEXT).clickAndWaitForNewWindow();
+            getObjectByTextContains(ALARM_CLICK_SAVE_TEXT).clickAndWaitForNewWindow();
             Thread.sleep(6000);
             gDevice.pressBack();
         }catch (Exception e){
@@ -52,11 +87,8 @@ public class AlarmAction extends VP4 {
             e.printStackTrace();
         }
     }
-    /*
-   * 选择手机在周几响铃。
-   * */
     public void repeatAlarmSetting(String monday,String tuesday,String wednesday,
-                                String thursday, String friday,String saturday,String sunday){
+                                String thursday, String friday,String saturday,String sunday){  //选择手机在周几响铃。
         try{
                 getObjectById(ALARM_FREQUENCY_FIELD_ID).clickAndWaitForNewWindow();
                 if(monday!=null){
@@ -90,7 +122,7 @@ public class AlarmAction extends VP4 {
     public void addRepeatAlarm(String monday,String tuesday,String wednesday,
                                 String thursday, String friday,String saturday,String sunday){
         try{
-            getObjectByTextContains("Add alarm").clickAndWaitForNewWindow();
+            getObjectByTextContains(ALARM_CLICK_ADD_ALARM_TEXT).clickAndWaitForNewWindow();
             repeatAlarmSetting(monday,tuesday,wednesday,thursday,friday,saturday,sunday);
             getObjectByTextContains(ALARM_CLICK_NEXT_TEXT).clickAndWaitForNewWindow();
             getObjectByTextContains(ALARM_CLICK_SAVE_TEXT).clickAndWaitForNewWindow();
