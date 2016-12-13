261dd541d32d60f2ee07db887a6ae3356095e9a4
jiana.fu <jiana.fu@ck-telecom.com>
2016-12-13 13:12:21 +0800

    add calculator page

---
+++ app/src/androidTest/java/doro/page/CalculatorPage.java
@@ -0,0 +1,28 @@
+package doro.page;
+
+/**
+ * Created by admin on 2016/12/12.
+ */
+
+public class CalculatorPage {
+    //数字键的类名
+    public static final String CALCULATOR_BUTTON_CLASS="android.widget.Button";
+
+    //加号的ResourceID
+    public static final String CALCULATOR_PLUSBUTTON_ID="com.doro.apps.calculator:id/button_plus";
+    //减号的ResourceID
+    public static final String CALCULATOR_MINUSBUTTON_ID="com.doro.apps.calculator:id/button_minus";
+    //乘号的ResourceID
+    public static final String CALCULATOR_MULTIBUTTON_ID="com.doro.apps.calculator:id/button_multi";
+    //除号的ResourceID
+    public static final String CALCULATOR_DIVISBUTTON_ID="com.doro.apps.calculator:id/button_divis";
+    //等号的ResourceID
+    public static final String CALCULATOR_EAQUESBUTTON_ID="com.doro.apps.calculator:id/button_equal";
+    //删除键的ResourceID
+    public static final String CALCULATOR_DELETEBUTTON_ID="com.doro.apps.calculator:id/button_delete";
+    //小数点的ResourceID
+    public static final String CALCULATOR_DOTBUTTON_ID= "com.doro.apps.calculator:id/button_dot";
+
+
+
+}
