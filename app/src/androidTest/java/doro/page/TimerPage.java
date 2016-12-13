d00ff4d8c2e6771022860b1ca39531038a46dd9a
wang.tao <wang.tao@ck-telecom.com>
2016-12-13 09:35:20 +0800

    Add TimerPage

---
+++ app/src/androidTest/java/doro/page/TimerPage.java
@@ -0,0 +1,16 @@
+package doro.page;
+
+/**
+ * Created by admin on 2016/12/12.
+ */
+
+public class TimerPage {
+    public static final String Timer_Class="android.widget.Button";
+    public static final String Reset_Text="Reset";
+    public static final String Set_Text="Set";
+    public static final String Reset_ID="com.doro.apps.timer:id/left_button";
+    public static final String Set_ID="com.doro.apps.timer:id/right_button";
+    public static final String Other_Button_ID="com.doro.apps.timer:id/fab";
+    public static final String Deletet_Class="android.widget.ImageButton";
+    public static final String Delete_ID="com.doro.apps.timer:id/delete";
+}
