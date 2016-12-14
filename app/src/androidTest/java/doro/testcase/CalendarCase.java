079760798608df960a3e89c875d13026036305a9
ZhangBo <bo_zhang_YL@163.com>
2016-12-14 20:17:31 +0800

    update calendar case

--- app/src/androidTest/java/doro/testcase/CalendarCase.java
+++ app/src/androidTest/java/doro/testcase/CalendarCase.java
@@ -6,6 +6,8 @@ import android.support.test.uiautomator.UiScrollable;
 import android.support.test.uiautomator.UiSelector;
 
 import com.squareup.spoon.Spoon;
+
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import ckt.base.VP4;
@@ -18,6 +20,10 @@ import static doro.page.CalednarPage.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class CalendarCase extends VP4 {
+    @BeforeClass
+    public static void initCalendar(){
+        unLock();
+    }
 
     @Test
     public void launchCalendarCase1() {//打开Calendar应用
@@ -27,9 +33,6 @@ public class CalendarCase extends VP4 {
     @Test
     public void createEventBeforeCase2(){//创建提前5分钟提醒事件
         openAppliction("Calen\u200Bdar");
-//       CalendarAction newCalendar =new CalendarAction("my new Calendar","Home","2017/01/01",
-//                "2017/01/05",null,"9:00","12:00", CALENDAR_RECURRENCE_EVERY_DAY,
-//                CALENDAR_REMINDER_TYPE_NOTIFICATION, CALENDAR_BEFORE_VALUE_1_HOUR);
         CalendarAction newCalendar =new CalendarAction("my new Calendar","Home",null,null,
                 null,null,null,null,null,null);
         newCalendar.addEvent();
