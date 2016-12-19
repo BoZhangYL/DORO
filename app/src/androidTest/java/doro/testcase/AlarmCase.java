7004c06199c050783ab3413fd35b6730e285a1ac
lingjiang.du <lingjiang.du@ck-telecom.com>
2016-12-19 20:06:08 +0800

    Merge branch 'master' of E:\androidWork\DORO with conflicts.

--- app/src/androidTest/java/doro/testcase/AlarmCase.java
+++ app/src/androidTest/java/doro/testcase/AlarmCase.java
@@ -1,14 +1,34 @@
 package doro.testcase;
 
+import android.support.test.runner.AndroidJUnit4;
+
 import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import ckt.base.VP4;
+import doro.action.AlarmAction;
 
 /**
  * Created by user on 2016/12/06   .
  */
-
-public class AlarmCase {
+@RunWith(AndroidJUnit4.class)
+public class AlarmCase extends VP4{
+//    @Test
+//    public void case1_enterAndExitAlarm(){
+//        openAppliction("Alarm");
+//        new AlarmAction().enterAndExitAlarm(5);
+//    }
+//    @Test
+//    public void case2_creatAlarm(){
+//        openAppliction("Alarm");
+//        new AlarmAction().addRepeatAlarm(0);
+//    }
     @Test
-    public void testA(){
-
+    public void case2_creatAlarm(){
+        openAppliction("Alarm");
+       // new AlarmAction().addRepeatAlarm("Monday",null,null,null,null,null,"Sunday");
+      new AlarmAction().addTimeAlarm("07:25");
+       // new AlarmAction().setRingtone("Silent");
+       // new AlarmAction().deleteOneAlarm();
     }
 }
--- app/src/androidTest/java/doro/testcase/SettingCase.java
+++ app/src/androidTest/java/doro/testcase/SettingCase.java
@@ -2,23 +2,29 @@ package doro.testcase;
 
 import android.support.test.runner.AndroidJUnit4;
 
-import com.squareup.spoon.Spoon;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import ckt.base.VP2;
+import ckt.base.VP4;
+import doro.action.SettingAction;
 
 /**
  * Created by elon on 2016/12/1.
  */
 @RunWith(AndroidJUnit4.class)
-public class SettingCase extends VP2 {
+public class SettingCase extends VP4 {
+//    @Test
+//    public void testA(){
+//        initDevice();
+//        gDevice.pressHome();
+//        clickByText("Settings");
+//        Spoon.screenshot("takeScreenShot","Settings");
+//    }
     @Test
-    public void testA(){
-        initDevice();
-        gDevice.pressHome();
-        clickByText("Settings");
-        Spoon.screenshot("takeScreenShot","Settings");
+    public void testB(){
+         openAppliction("Settings");
+         new SettingAction().dateAndTime();
+         //new SettingAction().setDate("2013-9-01");
+        new SettingAction().setTime("16:28");
     }
 }
