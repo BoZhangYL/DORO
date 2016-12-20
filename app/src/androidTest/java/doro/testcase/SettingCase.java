57e49e72585d8d966349bb1bcba6d4a76d9c40aa
lingjiang.du <lingjiang.du@ck-telecom.com>
2016-12-20 14:37:42 +0800

    empty

--- app/src/androidTest/java/doro/testcase/SettingCase.java
+++ app/src/androidTest/java/doro/testcase/SettingCase.java
@@ -2,29 +2,14 @@ package doro.testcase;
 
 import android.support.test.runner.AndroidJUnit4;
 
-import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import ckt.base.VP4;
-import doro.action.SettingAction;
 
 /**
  * Created by elon on 2016/12/1.
  */
 @RunWith(AndroidJUnit4.class)
 public class SettingCase extends VP4 {
-//    @Test
-//    public void testA(){
-//        initDevice();
-//        gDevice.pressHome();
-//        clickByText("Settings");
-//        Spoon.screenshot("takeScreenShot","Settings");
-//    }
-    @Test
-    public void testB(){
-         openAppliction("Settings");
-         new SettingAction().dateAndTime();
-         //new SettingAction().setDate("2013-9-01");
-        new SettingAction().setTime("16:28");
-    }
+
 }
