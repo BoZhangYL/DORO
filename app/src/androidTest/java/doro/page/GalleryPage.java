a53628cc6a26cff620165ff4d5a6812c5df1ba22
ZhangBo <bo_zhang_YL@163.com>
2016-12-27 17:11:49 +0800

    Create Gallery mode

---
+++ app/src/androidTest/java/doro/page/GalleryPage.java
@@ -0,0 +1,8 @@
+package doro.page;
+
+/**
+ * Created by bo.zhang on 2016/12/27   .
+ */
+
+public class GalleryPage {
+}
---
+++ app/src/androidTest/java/doro/testcase/GalleryCase.java
@@ -0,0 +1,8 @@
+package doro.testcase;
+
+/**
+ * Created by bo.zhang on 2016/12/27   .
+ */
+
+public class GalleryCase {
+}
