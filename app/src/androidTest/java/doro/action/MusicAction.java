6049168c15fe05bb942908afa4e156e4b42444b9
wang.tao <wang.tao@ck-telecom.com>
2016-12-29 17:32:54 +0800

    Add Music

--- app/src/androidTest/java/doro/action/MusicAction.java
+++ app/src/androidTest/java/doro/action/MusicAction.java
@@ -20,15 +20,15 @@ public class MusicAction extends VP4 {
         openAppliction(Music_Text);
     }
 
-    public void checkLanuchMusicResult() {
+    public void checkLanuchMusicResult() {//检查进入Music的结果
         try {
-            Assert.assertEquals("Case1_launchMusic(", Music_Iwantto_Text, getObjectById(Music_Iwantto_ID).getText());
+            Assert.assertEquals("Case1_launchMusic", Music_Iwantto_Text, getObjectById(Music_Iwantto_ID).getText());
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void playMusic() {
+    public void playMusic() {//播放Music
         try {
             String Title = getObjectById(Music_header_title_ID).getText();
             switch (Title) {
@@ -58,19 +58,82 @@ public class MusicAction extends VP4 {
                 e.printStackTrace();
         }
     }
-    public void pauseMusic(){
+
+    public void pauseMusic(){//暂停Music
         mDevice.pressBack();
         clickByPonit(360,1119);
     }
-    public void deleteMusic(){
-        try {
-            clickById(Music_Iwantto_ID);
-            scrollAndGetUIObject(Music_Delete_Text);
-            clickByText(Music_Delete_Text);
 
+    public void deleteMusic(){//删除一首Music
+        try {
+            UiObject ConfirmButton = getObjectById(Music_ConfirmButton_ID);
+            if(!ConfirmButton.exists()) {
+                UiObject IwanttoCloseButton=getObjectById(Music_IwanttoClose_ID);
+                if(!IwanttoCloseButton.exists()){
+                    clickById(Music_Iwantto_ID);
+                }
+                scrollAndGetUIObject(Music_Delete_Text);
+                clickByText(Music_Delete_Text);
+            }
+            Random i = new Random();
+            int j = i.nextInt(5);
+            clickByClass(Music_Title_Song_Class,j);
+            clickConfirm();
         }catch (Exception e){
             e.printStackTrace();
         }
     }
+    public void deleteAllMusic(){//删除所有Music
+        try {
+            UiObject ConfirmButton = getObjectById(Music_ConfirmButton_ID);
+            if(!ConfirmButton.exists()) {
+                UiObject IwanttoCloseButton = getObjectById(Music_IwanttoClose_ID);
+                if (!IwanttoCloseButton.exists()) {
+                    clickById(Music_Iwantto_ID);
+                }
+                scrollAndGetUIObject(Music_Delete_Text);
+                clickByText(Music_Delete_Text);
+            }
+            clickById(Music_DeleteAll_ID);
+            clickConfirm();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+    public void clickConfirm(){//点击Confirm键
+        try {
+            UiObject ConfirmButton = getObjectById(Music_ConfirmButton_ID);
+            ConfirmButton.clickAndWaitForNewWindow();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void clickOK(){//点击OK
+        try {
+            UiObject OKButton = getObjectByText(Music_OK_Text);
+            OKButton.clickAndWaitForNewWindow();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void clickCancel(){//点击Cancel
+        try {
+            UiObject CancelButton = getObjectByText(Music_Cancel_Text);
+            CancelButton.clickAndWaitForNewWindow();
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+    }
+    public void checkDeleteAllMusiceResult(){
+        try {
+            Assert.assertEquals("Case4_deleteMusic", Music_Noresults_Text, getObjectById(Music_Noresults_ID).getText());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
 
 }
+
