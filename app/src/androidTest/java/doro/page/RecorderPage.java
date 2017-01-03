a53da1f88c7ae9dc089f60be62055f4f2a9fe192
jiana.fu <jiana.fu@ck-telecom.com>
2017-01-03 10:14:28 +0800

    Add recorder page

---
+++ app/src/androidTest/java/doro/page/RecorderPage.java
@@ -0,0 +1,31 @@
+package doro.page;
+
+import static android.R.attr.id;
+
+/**
+ * Created by admin on 2016/12/22.
+ */
+
+public class RecorderPage {//数字键的类名
+    public static final String CALCULATOR_BUTTON_CLASS="android.widget.Button";
+
+    //播放列表的ResourceID
+    public static final String RECORDLIST_ID="com.doro.apps.soundrec:id/button";
+    //播放列表界面的menu的ResourceID
+    public static final String RECORDER_MENU_ID="com.doro.apps.soundrec:id/action_overflow_menu";
+    //播放列表menu界面删除选项的ResourceID
+    public static final String RECORDER_DELETE_ID="Delete sound(s)";
+    //选择全部录音的ResourceID
+    public static final String RECORDER_SELECTALL_ID="com.doro.apps.soundrec:id/selectallcheckbox";
+    //确认按钮的ResourceID
+    public static final String RECORDER_CONFIRM_ID="com.doro.apps.soundrec:id/confirm_text";
+    //OK键的ResourceID
+    public static final String RECORDER_OK_ID="com.doro.apps.soundrec:id/confirm_text";
+    //录音按钮的ResourceID
+    public static final String RECORDBUTTON_ID= "com.doro.apps.soundrec:id/btn_rec";
+    //停止并且保存录音按钮的ResourceID
+    public static final String RECORD_STOPBUTTON_ID= "com.doro.apps.soundrec:id/btn_stop";
+    //播放录音按钮的ResourceID
+    public static final String RECORD_PLAYBUTTON_ID="com.doro.apps.soundrec:id/icon_and_label";
+
+}
