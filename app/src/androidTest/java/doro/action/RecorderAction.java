9493d85ca1fa0810483f66c588b342b7fb2756a3
jiana.fu <jiana.fu@ck-telecom.com>
2017-01-03 10:04:52 +0800

    Add recorder action

--- app/src/androidTest/java/doro/action/RecorderAction.java
+++ app/src/androidTest/java/doro/action/RecorderAction.java
@@ -12,33 +12,74 @@ import static android.R.attr.id;
 import static android.R.attr.staticWallpaperPreview;
 import static android.view.View.Z;
 import static ckt.base.VP2.getObjectsById;
+import static doro.page.RecorderPage.RECORDBUTTON_ID;
+import static doro.page.RecorderPage.RECORDER_CONFIRM_ID;
+import static doro.page.RecorderPage.RECORDER_DELETE_ID;
+import static doro.page.RecorderPage.RECORDER_MENU_ID;
+import static doro.page.RecorderPage.RECORDER_OK_ID;
+import static doro.page.RecorderPage.RECORDER_SELECTALL_ID;
+import static doro.page.RecorderPage.RECORDLIST_ID;
+import static doro.page.RecorderPage.RECORD_PLAYBUTTON_ID;
+import static doro.page.RecorderPage.RECORD_STOPBUTTON_ID;
 
 /**
  * Created by admin on 2016/12/20.
  */
 
-public class RecorderAction extends VP4{
-    public void pressRecordButon(){//按录音按钮
+public class RecorderAction extends VP4 {
+    public void clearRecords() {//清空录音
         try {
-             getObjectById("com.doro.apps.soundrec:id/btn_rec").click();
-        }catch (Exception e){
+            getObjectById(RECORDLIST_ID).click();
+            waitTime(3);
+            getObjectById(RECORDER_MENU_ID).click();
+            waitTime(3);
+            getUiObjectByText(RECORDER_DELETE_ID).click();
+            waitTime(3);
+            getObjectById(RECORDER_SELECTALL_ID).click();
+            waitTime(3);
+            getObjectById(RECORDER_CONFIRM_ID).click();
+            waitTime(3);
+            getUiObjectByText(RECORDER_OK_ID).click();
+            waitTime(3);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
-    public void saveRecorder(){//按停止录音按钮
-        try {
-            getObjectById("com.doro.apps.soundrec:id/btn_stop").click();
-            waitTime(30);
-        }catch (Exception e){
-            e.printStackTrace();
-    }
-  }
-    public void checkRecord(){
-        try {
-        getObjectById("com.doro.apps.soundrec:id/button").click();
 
-        }catch (Exception e) {
+    public void pressRecordButon() {//按录音按钮
+        try {
+            getObjectById(RECORDBUTTON_ID).click();
+            waitTime(3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void saveRecorder() {//按停止录音按钮
+        try {
+            getObjectById(RECORD_STOPBUTTON_ID).click();
+            waitTime(3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void checkRecord() {//查看录音
+        try {
+            getObjectById(RECORDLIST_ID).click();
+            waitTime(3);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void playRecord() {
+        try {
+            getObjectById(RECORD_PLAYBUTTON_ID).click();
+            waitTime(3);
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
 }
+
