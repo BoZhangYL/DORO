5820d1770cc96ca37b30be51f3a83ec2e64005e4
jiana.fu <jiana.fu@ck-telecom.com>
2017-01-11 17:21:22 +0800

    Add CodeSafe page

---
+++ app/src/androidTest/java/doro/page/CodeSafePage.java
@@ -0,0 +1,9 @@
+package doro.page;
+
+/**
+ * Created by admin on 2017/1/10.
+ */
+
+public class CodeSafePage {
+    public static final String SETSAFECODE="Please set\n" + "a three digit code";
+}
