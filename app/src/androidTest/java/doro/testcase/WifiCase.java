0be5d863abe03336debaf8d78ad7f5043df91952
jiana.fu <jiana.fu@ck-telecom.com>
2017-01-11 10:43:37 +0800

    Add wifi case

--- app/src/androidTest/java/doro/testcase/WifiCase.java
+++ app/src/androidTest/java/doro/testcase/WifiCase.java
@@ -1,8 +1,26 @@
 package doro.testcase;
 
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import ckt.base.VP4;
+
+import static android.icu.lang.UCharacter.GraphemeClusterBreak.T;
+import static doro.page.WifiPage.WIFI_ONANDOFFID;
+
 /**
- * Created by user on 2017/01/09   .
+ * Created by admin on 2017/1/1.
  */
 
-public class WifiCase {
+public class WifiCase extends VP4{
+    @Test
+    public void OpenWifi(){//从菜单进入wifi
+        openAppliction("Settings");
+        clickByText("Wi‑Fi");
+        Assert.assertTrue("成从菜单进入wifi界面",getObjectById(WIFI_ONANDOFFID).exists());
+    }
+    @Test
+
+
 }
