c307c68e039fc1e084cfbb0e2b6d0f65aab0941d
ycbymy <835135815@qq.com>
2017-01-14 11:25:34 +0800

    change APPName

---
+++ app/src/androidTest/java/doro/page/APPMenuPage.java
@@ -0,0 +1,74 @@
+package doro.page;
+
+/**
+ * Created by Caibing.yin on 2017/1/12.
+ */
+
+public class APPMenuPage {
+    public static String AppNameList[] = {
+            "Alarm",
+            "Cal\u200Bcu\u200Blator", "Calen\u200Bdar","Camera", "Chrome", "Code safe", "Cont\u200Bacts",
+            "Dock mode", "Doro Connect & Care", "Doro Keyboard  ", "Doro Sel\u200Bection", "Downloads", "Drive", "Duo",
+            "Email", "Email",
+            "File Manager", "FM Radio",
+            "Gall\u200Bery", "Gmail", "Google",
+            "Inter\u200Bnet",
+            "Mag\u200Bnifier", "Maps", "Mess\u200Bages", "Music", "My Doro Manager",
+            "Notes",
+            "Phone", "Photos", "Play Movies & TV", "Play Music", "Play Store",
+            "QuickSupport",
+            "Rec\u200Border",
+            "Settings", "SIM Tool Kit",
+            "Timer", "Torch",
+            "Voice Search",
+            "Weath\u200Ber",
+            "YouTube",
+    };
+    public static String PkgNameList[]={
+            "com.doro.apps.alarm",
+            "com.doro.apps.calculator",
+            "com.doro.apps.calendar",
+            "com.mediatek.camera",
+            "com.android.chrome",
+            "Code safe",
+            "com.doro.apps.contacts",
+            "com.doro.apps.dock",
+            "com.doro.cnc.mobile",
+            "com.doro.inputmethod.latin",
+            "com.doro.apps.doroselection",
+            "com.android.documentsui",
+            "com.google.android.gms",
+            "com.google.android.apps.tachyon",
+            "com.doro.apps.email",
+            "com.doro.apps.email",
+            "com.mediatek.filemanager",
+            "com.android.fmradio",
+            "com.doro.apps.gallery",
+            "com.google.android.gm",
+            "com.google.android.googlequicksearchbox",
+            "com.doro.apps.internet",
+            "com.doro.apps.magnifier",
+            "com.google.android.apps.maps",
+            "com.doro.apps.messages",
+            "com.doro.apps.musicplayer",
+            "com.doro.cnc.mobile",
+            "com.doro.apps.notes",
+            "com.doro.apps.phone",
+            "com.google.android.apps.photos",
+            "com.mediatek.camera",
+            "com.google.android.music",
+           // "com.google.android.gms",
+            "com.google.android.play store",
+            "com.teamviewer.quicksupport.market",
+            "com.doro.apps.soundrec",
+            "com.doro.settings",
+            "com.android.stk - toolkit",
+            "com.doro.apps.timer",
+            "com.doro.apps.torch",
+            "com.google.android.googlequicksearchbox",
+            "com.doro.apps.weather",
+            "com.google.android.youtube",
+
+    };
+    public static final String More = "com.doro.apps.launcher3:id/more_actions";
+}
