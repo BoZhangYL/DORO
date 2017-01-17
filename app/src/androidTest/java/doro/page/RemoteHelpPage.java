a714b89cb84ae30e878cf6c97a4288339de3138b
jiana.fu <jiana.fu@ck-telecom.com>
2017-01-17 16:39:32 +0800

    Update remote help page

--- app/src/androidTest/java/doro/page/RemoteHelpPage.java
+++ app/src/androidTest/java/doro/page/RemoteHelpPage.java
@@ -5,5 +5,8 @@ package doro.page;
  */
 
 public class RemoteHelpPage {
-    public static final String RemoteHelpTitle="\"TeamViewer QuickSupport\"";
+    public static final String RemoteHelpTitle="TeamViewer QuickSupport";
+    public static final String SET="Set";
+    public static final String AN_ASSISTANCE_OPTION="An assistance option";
+    public static final String REMOTE_HELP="Remote help";
 }
