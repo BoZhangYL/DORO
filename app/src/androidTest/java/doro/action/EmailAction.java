b9c3d3ac7076ae5358192c2299ff570019b94908
ycbymy <835135815@qq.com>
2017-02-03 20:51:20 +0800

    update LogInEmail()方法

--- app/src/androidTest/java/doro/action/EmailAction.java
+++ app/src/androidTest/java/doro/action/EmailAction.java
@@ -53,11 +53,15 @@ public class EmailAction extends VP4 {
     public static Boolean IsExist_Email_account(String EmailAccount) throws UiObjectNotFoundException {
         MainAction.startApp(APPMenuPage.AppNameList[15]);
         waitTime(2);
-        if(text_exists("You can set up your account in just a few steps."))
+        if(text_exists("Write email")) {
+            clickById(EmailPage.EMAIL_I_WANT_TO);
+            clickByText("Manage accounts");
+            return text_exists(EmailAccount);
+        }
+        else if (
+            text_exists("You can set up your account in just a few steps.") ||
+                    !text_exists("Select an existing one in the list below:"));
             return false;
-        clickById(EmailPage.EMAIL_I_WANT_TO);
-        clickByText("Manage accounts");
-        return text_exists(EmailAccount);
     }
     //登录邮箱账号
     public static void LogInEmail(String account,String password,String nickname) throws UiObjectNotFoundException, IOException {
@@ -65,18 +69,89 @@ public class EmailAction extends VP4 {
         MainAction.startApp(APPMenuPage.AppNameList[15]);
         waitTime(3);
         if(!EmailAction.IsExist_Email_account(account)){
-            clickByText("Add account");
-            getObject2ById(EmailPage.EMAIL_ACCOUNT_INPUT).setText(account);
-            waitUntilFind(EmailPage.EMAIL_INPUT_OK_TEXT,2000);
-            clickById(EmailPage.EMAIL_INPUT_OK_TEXT);
-            waitUntilFind(EmailPage.EMAIL_PASSWORD_INPUT,10000);
-            getObject2ById(EmailPage.EMAIL_PASSWORD_INPUT).setText(password);
-            clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
-            waitTime(10);
-            clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
-            waitTime(5);
-            getObject2ById(EmailPage.EMAIL_ACCOUNT_NAME).setText(nickname);
-            clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+            //已有其他账号，再添加账号
+            if(text_exists("Add account")) {
+                clickByText("Add account");
+                //输入账号
+                getObject2ById(EmailPage.EMAIL_ACCOUNT_INPUT).setText(account);
+                waitTime(2);
+
+                //gmail邮箱比其他邮箱多个确认步骤，判断是否是gmail邮箱
+                if(account.endsWith("@gmail.com")) {
+                    waitTime(3);
+                    clickByText("Cancel");//gmail需要
+                    waitUntilFind(EmailPage.EMAIL_INPUT_OK_TEXT, 2000);
+                    clickById(EmailPage.EMAIL_INPUT_OK_TEXT);
+                    waitUntilFind(EmailPage.EMAIL_PASSWORD_INPUT, 20000);
+                    //输入密码
+                    getObject2ById(EmailPage.EMAIL_PASSWORD_INPUT).setText(password);
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    waitTime(10);
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    waitTime(5);
+                    getObject2ById(EmailPage.EMAIL_ACCOUNT_NAME).setText("");
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    //等待10s将收件箱刷新出来
+                    waitTime(10);
+                }
+                //非gmail邮箱
+                else {
+                    waitUntilFind(EmailPage.EMAIL_INPUT_OK_TEXT, 2000);
+                    clickById(EmailPage.EMAIL_INPUT_OK_TEXT);
+                    waitUntilFind(EmailPage.EMAIL_PASSWORD_INPUT, 20000);
+                    //输入密码
+                    getObject2ById(EmailPage.EMAIL_PASSWORD_INPUT).setText(password);
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    waitTime(10);
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    waitTime(5);
+                    getObject2ById(EmailPage.EMAIL_ACCOUNT_NAME).setText("");
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    //等待10s将收件箱刷新出来
+                    waitTime(10);
+                }
+            }
+            //一个账号都没有
+            else if(text_exists("You can set up your account in just a few steps.")){
+                //输入账号
+                getObject2ById(EmailPage.EMAIL_ACCOUNT_INPUT).setText(account);
+                waitTime(2);
+
+                //gmail邮箱比其他邮箱多个确认步骤，判断是否是gmail邮箱
+                if(account.endsWith("@gmail.com")) {
+                    waitTime(3);
+                    clickByText("Cancel");//gmail需要
+                    waitUntilFind(EmailPage.EMAIL_INPUT_OK_TEXT, 2000);
+                    clickById(EmailPage.EMAIL_INPUT_OK_TEXT);
+                    waitUntilFind(EmailPage.EMAIL_PASSWORD_INPUT, 20000);
+                    //输入密码
+                    getObject2ById(EmailPage.EMAIL_PASSWORD_INPUT).setText(password);
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    waitTime(10);
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    waitTime(5);
+                    getObject2ById(EmailPage.EMAIL_ACCOUNT_NAME).setText("");
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    //等待10s将收件箱刷新出来
+                    waitTime(10);
+                }
+                //非gmail邮箱
+                else {
+                    waitUntilFind(EmailPage.EMAIL_INPUT_OK_TEXT, 2000);
+                    clickById(EmailPage.EMAIL_INPUT_OK_TEXT);
+                    waitUntilFind(EmailPage.EMAIL_PASSWORD_INPUT, 20000);
+                    //输入密码
+                    getObject2ById(EmailPage.EMAIL_PASSWORD_INPUT).setText(password);
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    waitTime(10);
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    waitTime(5);
+                    getObject2ById(EmailPage.EMAIL_ACCOUNT_NAME).setText("");
+                    clickById(EmailPage.EMAIL_INPUT_OK_BUTTON);
+                    //等待10s将收件箱刷新出来
+                    waitTime(10);
+                }
+            }
         }
     }
     //退出所有邮箱账号
@@ -111,7 +186,7 @@ public class EmailAction extends VP4 {
         EmailAction.LogInEmail("chengduxike@hotmail.com","falcon@ckt2015","");
         gDevice.pressBack();
         clickById(EmailPage.EMAIL_WRITE_EMAIL);
-        waitUntilFind(EmailPage.EMAIL_ADDRESS,4000);
+        waitUntilFind(EmailPage.EMAIL_ADDRESS,6000);
         clickById(EmailPage.EMAIL_ADDRESS);
         //设置发送的地址
         waitTime(2);
