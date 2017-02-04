7a3d2ed907a6b2e35f356260d5364a3e8889b8b8
ycbymy <835135815@qq.com>
2017-02-04 20:41:37 +0800

    add page

---
+++ app/src/androidTest/java/doro/page/MainActivityPage.java
@@ -0,0 +1,12 @@
+package doro.page;
+
+/**
+ * Created by Caibing.yin on 2017/1/23.
+ */
+
+public class MainActivityPage {
+    public static final String NAME_INPUT = "com.doro.apps.contacts:id/edittext";
+    public static final String STREET_INPUT= "com.doro.apps.contacts:id/street";
+    public static final String STREET_NOTE = "com.doro.apps.contacts:id/note";
+    public static final String Container_view ="com.doro.apps.launcher3:id/container_view";
+}
