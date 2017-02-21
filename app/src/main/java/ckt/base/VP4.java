5b13e025a5fc70f252e1cd405890cbdb74110a1f
Zhang Bo <Bo_Zhang_YL@163.com>
2017-02-21 21:10:54 +0800

    Update

--- app/src/main/java/ckt/base/VP4.java
+++ app/src/main/java/ckt/base/VP4.java
@@ -335,10 +335,11 @@ public class VP4 extends VP2 {
     public static void initRent() {//清除recent
         try {
             pressKey("Home/menu");
-            while (getObjectById("com.android.systemui:id/task_view_thumbnail").exists()) {
+            /*while (getObjectById("com.android.systemui:id/task_view_thumbnail").exists()) {
                 scrollLeft(getObjectById("com.android.systemui:id/task_view_thumbnail"),
                         5);
-            }
+            }*/
+            getObjectByText("Close all").clickAndWaitForNewWindow();
         } catch (UiObjectNotFoundException e) {
             e.printStackTrace();
         }
