db158d8c1479382bc4ac2408aaa21cb94bacceb0
ZhangBo <bo_zhang_YL@163.com>
2017-02-24 10:00:31 +0800

    add clear Allow Page

--- app/src/androidTest/java/doro/action/MainAction.java
+++ app/src/androidTest/java/doro/action/MainAction.java
@@ -12,44 +12,51 @@ import doro.page.ViewPage;
 /**
  * Created by qiang.zhang on 2017/1/13.
  */
-public class MainAction extends VP4{
+public class MainAction extends VP4 {
     /*clear all apps*/
     public static void clearAllApp() throws UiObjectNotFoundException {
         initDevice();
         unLock();
         gDevice.pressMenu();
-        if (text_exists(ViewPage.NO_RECENT_ITEMS)){
+        if (text_exists(ViewPage.NO_RECENT_ITEMS)) {
             gDevice.pressHome();
-        }else {
-            waitUntilFind(ViewPage.CLEAR_APP_ID,5000);
+        } else {
+            waitUntilFind(ViewPage.CLEAR_APP_ID, 5000);
             while (id_exists(ViewPage.CLEAR_APP_ID)) {
                 clickById(ViewPage.CLEAR_APP_ID);
                 waitTime(1);
             }
         }
     }
+
     public static void startApp(String AppName) throws UiObjectNotFoundException {
         gDevice.pressHome();
-        waitUntilFind(ViewPage.MORE_ACTIONS_ID,5000);
+        waitUntilFind(ViewPage.MORE_ACTIONS_ID, 5000);
         clickById(ViewPage.MORE_ACTIONS_ID);
         //推荐的app list中是否存在
-        if (text_exists(AppName)){
+        if (text_exists(AppName)) {
             clickByText(AppName);
-        }else{
+        } else {
             //滑动查找App
-            waitUntilFind(ViewPage.APP_LIST_SEARCH,10000);
+            waitUntilFind(ViewPage.APP_LIST_SEARCH, 10000);
             clickById(ViewPage.APP_LIST_SEARCH);
             getObject2ById(ViewPage.SEARCH_BOX_INPUT).clear();
             waitTime(2);
-            getObject2ById(ViewPage.SEARCH_BOX_INPUT).setText(AppName.substring(0,3));
+            getObject2ById(ViewPage.SEARCH_BOX_INPUT).setText(AppName.substring(0, 3));
             waitTime(3);
             //gDevice.pressBack();
-            waitUntilFind(LAUNCH3_APP,30000);
-            clickByIdText(LAUNCH3_APP,AppName);
-            logger.info("current-package is - "+gDevice.getCurrentPackageName());
+            waitUntilFind(LAUNCH3_APP, 30000);
+            clickByIdText(LAUNCH3_APP, AppName);
+            logger.info("current-package is - " + gDevice.getCurrentPackageName());
+        }
+        if (getObjectByText("ALLOW").exists()) {
+            while (getObjectByText("ALLOW").exists()) {
+                getObjectByText("ALLOW").clickAndWaitForNewWindow();
+            }
         }
         waitTime(5);
     }
+
     /**
      * 启动FILE MANAGER
      */
@@ -58,10 +65,11 @@ public class MainAction extends VP4{
         gDevice.pressHome();
         waitTime(2);
         gDevice.executeShellCommand("am start -n com.mediatek.filemanager/.FileManagerOperationActivity");
-        gDevice.wait(Until.findObject(By.pkg("com.mediatek.filemanager")),40000);
+        gDevice.wait(Until.findObject(By.pkg("com.mediatek.filemanager")), 40000);
         String pkg = gDevice.getCurrentPackageName();
-        logger.info("current-package:"+pkg);
+        logger.info("current-package:" + pkg);
     }
+
     /**
      * 关闭文件管理器
      */
@@ -77,6 +85,7 @@ public class MainAction extends VP4{
             logger.info("stop filemanager-Fail");
         }
     }
+
     /**
      * 启动FILE MANAGER
      */
@@ -85,10 +94,11 @@ public class MainAction extends VP4{
         gDevice.pressHome();
         waitTime(2);
         gDevice.executeShellCommand("am start -n com.doro.apps.messages/.ui.conversationlist.ConversationListActivity");
-        gDevice.wait(Until.findObject(By.pkg("com.doro.apps.messages")),40000);
+        gDevice.wait(Until.findObject(By.pkg("com.doro.apps.messages")), 40000);
         String pkg = gDevice.getCurrentPackageName();
-        logger.info("current-package:"+pkg);
+        logger.info("current-package:" + pkg);
     }
+
     /**
      * 关闭文件管理器
      */
