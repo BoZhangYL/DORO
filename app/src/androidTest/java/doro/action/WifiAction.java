dc30d8e76d3335e99086d7bf7db7219e9754ba2a
ZhangBo <bo_zhang_YL@163.com>
2017-06-05 13:58:34 +0800

    Update connectWifi

--- app/src/androidTest/java/doro/action/WifiAction.java
+++ app/src/androidTest/java/doro/action/WifiAction.java
@@ -17,33 +17,33 @@ import static doro.page.WifiPage.WIFI_ONANDOFFID;
  * Created by admin on 2017/1/10.
  */
 
-public class WifiAction extends VP4{
-    public void turnOnWifi(boolean yesOrNo){
-        if(yesOrNo){
-            if(!getObjectByText(TURN_ON_WIFI).exists()){//判断是否需要开启wifi
+public class WifiAction extends VP4 {
+    public void turnOnWifi(boolean yesOrNo) {
+        if (yesOrNo) {
+            if (!getObjectByText(TURN_ON_WIFI).exists()) {//判断是否需要开启wifi
                 clickById(WIFI_ONANDOFFID);
             }
-        }else{
-            if(getObjectByText(TURN_ON_WIFI).exists()){
+        } else {
+            if (getObjectByText(TURN_ON_WIFI).exists()) {
                 clickById(WIFI_ONANDOFFID);
             }
         }
     }
-    public void connectWifi(String name,String password){
-        try{
-            if(getObjectByText(name).exists()&&getObjectByText(CONNECTED_WIFI).exists()) {//判断所需连接wifi是否存在
+
+    public void connectWifi(String name, String password) {
+        try {
+            if (!getObjectByText(name).exists() && !getObjectByText(CONNECTED_WIFI).exists()) {//判断所需连接wifi是否存在
                 clickByText(name);
-                clickByText(FORGET_WIFI);
+                WifiAction.getPassword(password);//输入密码
+                clickByText(CONNECT_WIFI);//连接
+                waitTime(10);
+                Assert.assertTrue("未能成功连接wifi", getObjectByText(CONNECTED_WIFI).exists());//判断连接是否成功
             }
-            clickByText(name);
-            WifiAction.getPassword(password);//输入密码
-            clickByText(CONNECT_WIFI);//连接
-            waitTime(10);
-            Assert.assertTrue("未能成功连接wifi",getObjectByText(CONNECTED_WIFI).exists());//判断连接是否成功
-        }catch(Exception e){
+        } catch (Exception e) {
             e.printStackTrace();
         }
     }
+
     private static void doroInput(UiObject object, String text) {//输入键盘的操作
         try {
             object.click();
@@ -53,7 +53,9 @@ public class WifiAction extends VP4{
             e.printStackTrace();
         }
     }
+
     public static void getPassword(String Passwords) {//输入密码
         UiObject PasswordBox = getObjectById(INPUT_PASSWORDBOX);
-        doroInput(PasswordBox,Passwords);}
+        doroInput(PasswordBox, Passwords);
+    }
 }
