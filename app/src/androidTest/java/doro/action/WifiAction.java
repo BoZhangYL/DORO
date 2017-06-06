657002b167c7cc6d5ae43fc04bea9c8110287fe0
ZhangBo <bo_zhang_YL@163.com>
2017-06-06 10:17:21 +0800

    Update

--- app/src/androidTest/java/doro/action/WifiAction.java
+++ app/src/androidTest/java/doro/action/WifiAction.java
@@ -36,21 +36,29 @@ public class WifiAction extends VP4 {
     }
 
     public void connectWifi(String name, String password) {
+
         UiCollection WIFIList = new UiCollection(new UiSelector()
                 .resourceId("com.android.settings:id/list"));
+        try {
+            while (WIFIList.getChildCount() < 3) {
+                waitTime(1);
+            }
+        } catch (UiObjectNotFoundException e) {
+            e.printStackTrace();
+        }
         String WifiName = null;
         String ConnectState = null;
         //String WiFiSignal = null;
         String nextWifiName = null;
         try {
             for (int i = 0; i < WIFIList.getChildCount(); i++) {
-                UiObject CurrentWifi = WIFIList.getChildByInstance(new UiSelector()
-                        .className("android.widget.LinearLayout"), i);
+                UiObject CurrentWifi = WIFIList.getChild(new UiSelector().index(i));
                 String[] wifiDescrption = CurrentWifi.getContentDescription().split(",");
                 WifiName = wifiDescrption[0];
-                ConnectState = wifiDescrption[1];
+                //ConnectState = wifiDescrption[1];
                 //WiFiSignal = wifiDescrption[2];
                 if (WifiName.equals(name)) {
+                    ConnectState = wifiDescrption[1];
                     if (!ConnectState.equals("Connected")) {
                         clickByText(name);
                         WifiAction.getPassword(password);//输入密码
@@ -58,7 +66,7 @@ public class WifiAction extends VP4 {
                         waitTime(10);
                         Assert.assertTrue("未能成功连接wifi", getObjectByText(CONNECTED_WIFI).exists());//判断连接是否成功
                         break;
-                    }
+                    } else break;
                 } else if ((i == WIFIList.getChildCount() - 1) && !(WifiName.equals(nextWifiName))) {
                     scrollForward(20);
                     connectWifi(name, password);
